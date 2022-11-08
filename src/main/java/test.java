c938daf016e5f53446919c76ec241140bd1217ad
lihaitian <1762438489@qq.com>
2022-11-08 16:59:43 +0800

    master commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("第一次git!!!!");
         System.out.println("合并代码");
+        System.out.println("master");
     }
 }
